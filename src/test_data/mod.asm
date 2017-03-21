f5c0325c6fc7b7ed9998bf984569c3bae815df40
Satya Bodduluri <skbodduluri@gmail.com>
2017-03-21 17:54:59 -0400

    got mod working with d

--- src/test_data/mod.asm
+++ src/test_data/mod.asm
@@ -2,15 +2,15 @@
 ADD R1, R3, #0 ;
 ; Mod expects inputs in R1 and R2
 ; Mod Step
-; TCS R2
-; TCDH R1
+TCS R2
+TCDH R1
 MOD_SR SLL R0, R1, #4  ; R5 <- p << 4
 SLL R6, R1, #1  ; R6 <- p << 1
-ADD R0, R0, R6  ; R5 <- p << 4 + p << 1
+ADD R0, R0, R6  ; R0 <- p << 4 + p << 1
 ADD R3, R1, #0  ; R3 <- R1
 ADD R0, R0, R3  ; R5 <- p << 4 + p << 1 + p
 ADD R0, R0, R2  ; R5 <- p << 4 + p << 1 + p + r
-; ADD R2, R0, #0
-; ADD R1, R4, #0
-; SUB R0, R1, R2
+ADD R2, R0, #0
+ADD R1, R4, #0
+SUB R0, R1, R2
 DONE             ; Result placed in R0 TODO need to add signed and 255 bit split
